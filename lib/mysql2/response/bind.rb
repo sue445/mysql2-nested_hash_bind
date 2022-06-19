fb9ff46959167d934b525ae903721d4e1f4d5fbf
sue445 <sue445@sue445.net>
2022-06-19 15:18:55 +0900

    Add QueryExtension

--- lib/mysql2/response/bind.rb
+++ lib/mysql2/response/bind.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "bind/version"
+require_relative "bind/query_extension"
 
 module Mysql2
   module Response
---
+++ spec/mysql2/response/bind/query_extension_spec.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+RSpec.describe Mysql2::Response::Bind::QueryExtension do
+end
